b6ca52ae0980b0f07bad4cfe6f41b0e54f30bdf1
MahammadShahid <32709331+MahammadShahid@users.noreply.github.com>
2020-04-11 12:35:59 +0530

    Add files via upload

---
+++ java script/common.js
@@ -0,0 +1,30 @@
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-60px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
+
+
+
+/* ....................................................login popup............................................*/
+
+
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+
+
+
